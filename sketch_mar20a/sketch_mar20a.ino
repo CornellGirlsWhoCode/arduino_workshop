2bdb2bee728b90c87de8c101ab4d101ca9388dae
CornellGirlsWhoCode <47289030+CornellGirlsWhoCode@users.noreply.github.com>
2022-03-20 15:50:26 -0400

    Lily's!

--- sketch_mar20a/sketch_mar20a.ino
+++ sketch_mar20a/sketch_mar20a.ino
@@ -1,27 +1,37 @@
 int switchState = 0;
-void setup() {
+
+void setup() 
+{
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(2, INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4, OUTPUT );
+  pinMode(5,OUTPUT);
+  pinMode(2,INPUT);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
   switchState = digitalRead(2);
-  if (switchState == LOW) {
-    digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
+  
+
+  if(switchState == LOW)
+  {
+  
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
   }
-  else {
+  
+  else
+  {
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     digitalWrite(5, HIGH);
-    delay(250);
+  
+    delay(500);
     digitalWrite(4, HIGH);
     digitalWrite(5, LOW);
-    delay(250);
+    delay(500);
   }
 }
